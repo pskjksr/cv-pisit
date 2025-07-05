32ec8d0c0d051a72a7bc221a27a94c79ba0d6acd
pskjksr <friendzazaza555@gmail.com>
2025-07-05 20:27:39 +0700

    change friend

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
         <div className="hidden md:flex gap-6 text-sm font-medium">
           <a href="#">About</a>
           <a href="#">Skills</a>
-          <a href="#">Projects</a>
+          <a href="#">friend</a>
         </div>
         <a href="#" className="bg-black text-white px-4 py-2 rounded text-sm">Contact me</a>
       </nav>
